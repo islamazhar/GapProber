ce51461c66695d126bb3187df6dc41ab9a6df30c
Mazharul-islam-nipu <mazhar.buet11@gmail.com>
2019-01-20 19:23:06 +0600

    first commit

---
+++ cgal.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define NUM_THREADS 32
+#define SEED 94703
+
+
+void myitoa(int value, char *s, int base)
+{
+	sprintf(s,"%d",value);
+}
+
+/*
+char * toupper(char *contig)
+{
+	long int i=0;
+	char c;
+	while (contig[i])
+  	{
+    		c=contig[i];
+    		contig[i]=toupper(c);
+    		i++;
+  	}
+	return contig;
+}
+*/
+
+
